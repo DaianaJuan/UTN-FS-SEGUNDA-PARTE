2c0ddbbc93039b3f5c58a9183c2c745de1e629b2
DaiJuan <daianazajuan@gmail.com>
2024-07-26 23:43:43 -0300

    wsp

--- react-proyecto/src/App.jsx
+++ react-proyecto/src/App.jsx
@@ -1,34 +1,14 @@
 import React, {useState} from 'react'
-import { chat } from './data.js'
-import ListaChat from './componentes/ListaChat/ListaChat'
+import Mensajeria from './componentes/mensajeria'
 import './style.css'
-import Footer from './componentes/Footer/Footer.jsx'
-import Header from './componentes/Header/Header.jsx'
+
 
 
 function App() {
 
-  const [listaMensajes, setlistaMensajes] = useState(chat)
-  const [formMensaje, setFormMensaje] = useState({contenido: ''})
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const nuevoMensaje = {
-      autor: 'Yo', 
-      contenido: formMensaje.contenido,
-      fecha: '14:15 p m', 
-      estado: 'entregado', 
-      id: listaMensajes.length + 1
-    }
-    setlistaMensajes([...listaMensajes, nuevoMensaje])
-    setFormMensaje({contenido: ''})
-  }
-
   return (
     <>
-      <Header/>
-      <ListaChat mensajes={listaMensajes}/>
-      <Footer formMensaje={formMensaje} setFormMensaje={setFormMensaje} handleSubmit={handleSubmit}/>
+      <Mensajeria/>
     </>
   )
 }
---
+++ react-proyecto/src/componentes/mensajeria.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { chat } from '../data.js'
+import ListaChat from './ListaChat/ListaChat'
+import Footer from './Footer/Footer.jsx'
+import Header from './Header/Header.jsx'
+import './mensajeria.css'
+
+
+function Mensajeria() {
+
+    const [listaMensajes, setlistaMensajes] = useState(chat)
+    const [formMensaje, setFormMensaje] = useState({ contenido: '' })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const nuevoMensaje = {
+            autor: 'Yo',
+            contenido: formMensaje.contenido,
+            fecha: '14:15 p m',
+            estado: 'entregado',
+            id: listaMensajes.length + 1
+        }
+        setlistaMensajes([...listaMensajes, nuevoMensaje])
+        setFormMensaje({ contenido: '' })
+    }
+
+    return (
+        <div className='contenedor-mensajeria'>
+            <Header />
+            <ListaChat mensajes={listaMensajes} />
+            <Footer formMensaje={formMensaje} setFormMensaje={setFormMensaje} handleSubmit={handleSubmit} />
+        </div>
+    )
+}
+
+export default Mensajeria
